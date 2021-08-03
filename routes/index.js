a194f0ce47631a8de25b1d93c76618e7e74822c9
SerhiiOrSoratik <87651890+SerhiiOrSoratik@users.noreply.github.com>
2021-08-03 13:47:28 +0300

    /dashboard

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const todolist = require('./todolist.routes');
+const dashboard = require('./dashboard.routes');
 
 router.use('/todolist', todolist);
+router.use('/dashboard', dashboard);
 
 module.exports = router;
